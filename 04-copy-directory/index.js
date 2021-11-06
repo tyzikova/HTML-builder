c0438efe214f865b5832e81b9a7dc85e97521f3f
tyzikova <tyzikova27@mail.ru>
2021-11-06 15:56:54 +0300

    feat:add solution 04 index.js (Sat, Nov 06, 2021 03:56:54 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir(){
+  const file = path.join(__dirname, 'files');
+  const copyFolder = path.join(__dirname, 'files-copy');
+
+  await fs.mkdir(copyFolder, {recursive: true});
+  const items = await fs.readdir(file, {withFileTypes: true}, (err) => {
+    if(err) process.stdout.write(err);
+  });
+  for (let i = 0; i< items.length; i++){
+    const from = path.join(file, items[i].name);
+    const to = path.join(copyFolder, items[i].name);
+    if(!items[i].isDirectory()){
+      fs.copyFile(from, to);
+    }
+  }
+}
+copyDir();
+
+
+/*solution with promises mothod*/
+
+// const fs = require('fs/promises');
+// const path = require('path');
+
+// async function copyDir(){
+//   const file = path.join(__dirname, 'files');
+//   const fileCopy = path.join(__dirname, 'files-copy');
+
+//   fs.mkdir(fileCopy, {recursive: true});
+//   const files = await fs.readdir(file, {withFileTypes: true});
+
+//   for (let i = 0; i < files.length; i++){
+//     if (files[i].isFile()){
+//       const from = path.join(file, files[i].name);
+//       const to = path.join(fileCopy, files[i].name);
+//       fs.copyFile(from, to);
+//     }
+//   }
+// }
+// copyDir(); 
